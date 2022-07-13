e79d6389a5e9cfc1a8a44cf7e5f6bfa71be714de
Halit Oskan <halit.oskan@and.digital>
2022-07-13 16:12:51 +0100

    getting ready for stack nav

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import RestScreen from './src/features/restaurants/screens/RestScreen';
-import Map from './src/features/restaurants/screens/Map';
-import Settings from './src/features/restaurants/screens/Settings';
 import { RestaurantContextProvider } from './src/services/restaurants/restaurant.context';
 import LocationContextProvider from './src/services/location/LocationContext';
 import {
@@ -20,30 +14,7 @@ import {
   Lato_400Regular,
   Lato_700Bold,
 } from '@expo-google-fonts/lato';
-
-const Tab = createBottomTabNavigator();
-
-const TabIcons = {
-  Restaurants: 'restaurant',
-  Map: 'map',
-  Settings: 'settings',
-};
-
-// *** More functional approach - function returning function ***
-// const tabBarIcon =
-//   (iconName) =>
-//   ({ size, color }) => {
-//     return <Ionicons name={iconName} size={size} color={color} />;
-//   };
-
-const setScreenOptions = ({ route }) => {
-  const iconName = TabIcons[route.name];
-  return {
-    tabBarIcon: ({ size, color }) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-  };
-};
+import { Navigation } from './src/infrastracture/navigation';
 
 export default function App() {
   const [OswaldFontLoaded] = useOswaldFont({
@@ -62,39 +33,9 @@ export default function App() {
   return (
     <>
       <ExpoStatusBar style='auto' />
-
       <LocationContextProvider>
         <RestaurantContextProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              initialRouteName='RestScreen'
-              screenOptions={setScreenOptions}
-              // ****** Claimed to be Simplified ??? ******
-              // screenOptions={({ route }) => ({
-              //   tabBarIcon: ({ color, size }) => {
-              //     let iconName;
-              //     if (route.name === 'Restaurants') {
-              //       iconName = 'restaurant';
-              //     } else if (route.name === 'Map') {
-              //       iconName = 'map';
-              //     } else if (route.name === 'Settings') {
-              //       iconName = 'settings';
-              //     }
-              //     return <Ionicons name={iconName} size={size} color={color} />;
-              //   },
-              // })}
-              tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-                // Can Add style here
-                // style: {},
-              }}
-            >
-              <Tab.Screen name='Restaurants' component={RestScreen} />
-              <Tab.Screen name='Map' component={Map} />
-              <Tab.Screen name='Settings' component={Settings} />
-            </Tab.Navigator>
-          </NavigationContainer>
+          <Navigation />
         </RestaurantContextProvider>
       </LocationContextProvider>
     </>
---
+++ src/infrastracture/navigation/AppNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Map from '../../features/restaurants/screens/Map';
+import Settings from '../../features/restaurants/screens/Settings';
+import { Ionicons } from '@expo/vector-icons';
+import RestaurantNavigator from './RestaurantNavigator';
+
+const Tab = createBottomTabNavigator();
+
+const TabIcons = {
+  Restaurants: 'restaurant',
+  Map: 'map',
+  Settings: 'settings',
+};
+
+const setScreenOptions = ({ route }) => {
+  const iconName = TabIcons[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='RestScreen'
+        screenOptions={setScreenOptions}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name='Restaurants' component={RestaurantNavigator} />
+        <Tab.Screen name='Map' component={Map} />
+        <Tab.Screen name='Settings' component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
