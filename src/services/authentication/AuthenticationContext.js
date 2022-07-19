5a612cc3afbcab94102c86b0fb2ac41155be3fd9
Halit Oskan <halit.oskan@and.digital>
2022-07-19 11:17:02 +0100

    varying

---
+++ src/services/authentication/AuthenticationContext.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect, createContext } from 'react';
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  onAuthStateChanged,
+  FacebookAuthProvider,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+
+export const AuthenticationContext = createContext();
+
+export const AuthenticationContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [isAuthLoading, setIsAuthLoading] = useState(false);
+
+  const onLogin = (email = 'email@email.com', password = 'password1234') => {
+    setIsAuthLoading(true);
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('USER', user);
+        setUser(user);
+        setIsAuthLoading(false);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('ERROR', errorCode, errorMessage);
+        setError(errorMessage);
+      });
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={(user, isAuthLoading, error, onLogin)}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
+
+// createUserWithEmailAndPassword(auth, 'email@email.com', 'password1234')
+// .then((userCredentials) => {
+//   const user = userCredentials.user;
+//   console.log('USER', user);
+// })
+// .catch((error) => {
+//   const errorCode = error.code;
+//   const errorMessage = error.message;
+//   console.log('ERROR', errorCode, errorMessage);
+// });
